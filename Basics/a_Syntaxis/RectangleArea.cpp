d095ec60c58cc0751114b9658d9acc0589482850
KaPrimov <k.primov92@gmail.com>
2018-09-02 02:35:14 +0300

    Added first lab

---
+++ Basics/a_Syntaxis/RectangleArea.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rectangleSideA, rectangleSideB;
+
+    cin >> rectangleSideA >> rectangleSideB;
+
+    const long area = rectangleSideA * rectangleSideB;
+
+    cout << area << endl;
+    return 0;
+}
